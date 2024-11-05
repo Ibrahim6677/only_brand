09893fcb0072f732c60c478dd70d19edec9c0e5d
ibrahim <ibrahimsaad6677@gmail.com>
2024-11-06 00:06:26 +0200

    Done

--- src/SecondPage/Second.jsx
+++ src/SecondPage/Second.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/autoplay';
-import { Autoplay } from 'swiper/modules'; // تأكد من استخدام "swiper/modules" بدلًا من "swiper"
+import { Autoplay } from 'swiper/modules';
 
 const BrandSwiper = () => {
   return (
-    <Swiper
-      modules={[Autoplay]}
-      spaceBetween={10} // المسافة بين العناصر
-      slidesPerView={4} // عدد العناصر المرئية
-      loop={true}
-      autoplay={{
-        delay: 1000,
-        disableOnInteraction: false,
-      }}
-    >
-      <h2>FEEL CONFIDENT AND BEAUTIFUL IN EVERY OUTFIT</h2>
-      <SwiperSlide><img src="/images/image 13.png" alt="Brand 1" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 14.png" alt="Brand 2" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 15.png" alt="Brand 3" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 16.png" alt="Brand 4" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 17.png" alt="Brand 5" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 18.png" alt="Brand 6" /></SwiperSlide>
-      <SwiperSlide><img src="/images/image 19.png" alt="Brand 7" /></SwiperSlide>
-    </Swiper>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h2 style={{margin: '20px 0', padding: '20px 0'}}>FEEL CONFIDENT AND BEAUTIFUL IN EVERY OUTFIT</h2>
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={5} // المسافة بين العناصر
+        slidesPerView={5} // عدد العناصر المرئية
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 13.png" width={150} alt="Brand 1" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 14.png" width={150} alt="Brand 2" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 15.png" width={150} alt="Brand 3" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 16.png" width={150} alt="Brand 4" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 17.png" width={150} alt="Brand 5" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 18.png" width={150} alt="Brand 6" /></SwiperSlide>
+        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 19.png" width={150} alt="Brand 7" /></SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
-export default BrandSwiper;
+export default BrandSwiper;
