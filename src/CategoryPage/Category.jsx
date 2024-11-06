2187e09d9f6e637a170a7ff4140e31956d6494e4
ibrahim <ibrahimsaad6677@gmail.com>
2024-11-06 21:23:09 +0200

    UPDATE

--- src/CategoryPage/Category.jsx
+++ src/CategoryPage/Category.jsx
@@ -7,27 +7,86 @@ import { Autoplay } from 'swiper/modules';
 const Category = () => {
   return (
     <div style={{ textAlign: 'center', padding: '40px', margin: '20px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
-      <h2 style={{margin: '20px 0', padding: '20px 0'}}>Shop By Categories</h2>
+      <h2 style={{ margin: '20px 0', padding: '20px 0' }}>Shop By Categories</h2>
       <Swiper
         modules={[Autoplay]}
         spaceBetween={5} // المسافة بين العناصر
-        slidesPerView={5} // عدد العناصر المرئية
         loop={true}
         autoplay={{
           delay: 3000,
           disableOnInteraction: false,
         }}
+        breakpoints={{
+          320: {
+            slidesPerView: 2, // عدد العناصر المرئية للشاشات الصغيرة جداً
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 3, // عدد العناصر المرئية للشاشات المتوسطة
+            spaceBetween: 15,
+          },
+          768: {
+            slidesPerView: 4, // عدد العناصر المرئية للأجهزة اللوحية
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 5, // عدد العناصر المرئية للشاشات الكبيرة
+            spaceBetween: 30,
+          },
+        }}
       >
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 36.png" width={150} alt="Brand 1" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 33.png" width={150} alt="Brand 2" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 34.png" width={150} alt="Brand 3" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 43.png" width={150} alt="Brand 4" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 42.png" width={150} alt="Brand 5" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 18.png" width={150} alt="Brand 6" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 27.png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 75(2).png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 45.png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: '#FFFAFA'}}><img src="/images/image 46.png" width={150} alt="Brand 7" /></SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 36.png" width={150} alt="Brand 1" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 33.png" width={150} alt="Brand 2" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 34.png" width={150} alt="Brand 3" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 43.png" width={150} alt="Brand 4" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 42.png" width={150} alt="Brand 5" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 20.png" width={150} alt="Brand 6" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 27.png" width={150} alt="Brand 7" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 75(2).png" width={150} alt="Brand 8" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 45.png" width={150} alt="Brand 9" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 46.png" width={150} alt="Brand 10" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 41.png" width={150} alt="Brand 11" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 37.png" width={150} alt="Brand 12" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px' }}>
+          <img style={{backgroundColor: '#FFFAFA', borderRadius: '15px', padding: '20px 40px'}} src="/images/image 50.png" width={150} alt="Brand 13" />
+          <p style={{textAlign: 'center', fontSize: '20px', fontWeight: '500', fontFamily: 'Geist', textTransform: 'capitalize'}}>T-Shirts</p>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/SecondPage/Second.jsx
+++ src/SecondPage/Second.jsx
@@ -7,28 +7,67 @@ import { Autoplay } from 'swiper/modules';
 const Second = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px', margin: '20px' }}>
-      <h2 style={{margin: '20px 0', padding: '20px 0'}}>FEEL CONFIDENT AND BEAUTIFUL IN EVERY OUTFIT</h2>
+      <h2 style={{ margin: '0 0 20px 0' }}>FEEL CONFIDENT AND BEAUTIFUL IN EVERY OUTFIT</h2>
       <Swiper
         modules={[Autoplay]}
         spaceBetween={5} // المسافة بين العناصر
-        slidesPerView={5} // عدد العناصر المرئية
         loop={true}
         autoplay={{
           delay: 1000,
           disableOnInteraction: false,
         }}
+        breakpoints={{
+          320: {
+            slidesPerView: 2, // عدد العناصر المرئية للشاشات الصغيرة جدا
+            spaceBetween: 5,
+          },
+          640: {
+            slidesPerView: 3, // عدد العناصر المرئية للشاشات المتوسطة
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 4, // عدد العناصر المرئية للأجهزة اللوحية
+            spaceBetween: 15,
+          },
+          1024: {
+            slidesPerView: 5, // عدد العناصر المرئية للشاشات الكبيرة
+            spaceBetween: 20,
+          },
+        }}
       >
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 13.png" width={150} alt="Brand 1" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 14.png" width={150} alt="Brand 2" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 15.png" width={150} alt="Brand 3" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 16.png" width={150} alt="Brand 4" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 17.png" width={150} alt="Brand 5" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 18.png" width={150} alt="Brand 6" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 19.png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 66.png" width={150} alt="Brand 8" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 67.png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 57.png" width={150} alt="Brand 7" /></SwiperSlide>
-        <SwiperSlide style={{borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)'}}><img src="/images/image 47.png" width={150} alt="Brand 7" /></SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 13.png" width={150} alt="Brand 1" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 14.png" width={150} alt="Brand 2" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 15.png" width={150} alt="Brand 3" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 16.png" width={150} alt="Brand 4" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 17.png" width={150} alt="Brand 5" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 18.png" width={150} alt="Brand 6" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 19.png" width={150} alt="Brand 7" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 66.png" width={150} alt="Brand 8" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 67.png" width={150} alt="Brand 7" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 57.png" width={150} alt="Brand 7" />
+        </SwiperSlide>
+        <SwiperSlide style={{ borderRadius: '15px', backgroundColor: 'var(--Black-100, #E6E6E6)' }}>
+          <img src="/images/image 47.png" width={150} alt="Brand 7" />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
