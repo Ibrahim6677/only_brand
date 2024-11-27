a43a278bd998fa3a134de874dc1fb6e524d5203a
ibrahim <ibrahimsaad6677@gmail.com>
2024-11-27 22:02:39 +0200

    finishing

--- Router/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const usrcontrollers = require("../controllers/user.js");
-const router = express.Router();
-
-router.post("/api/users/register", usrcontrollers.register);
-router.post("/api/users/login", usrcontrollers.login);
-
-module.exports = router;
--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export async function ProductsData() {
-  const products = await axios.get('http://localhost:3000/api/product');
-  console.log(products.data);
-  return products;
-}
