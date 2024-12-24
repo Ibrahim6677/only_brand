e0bfc0be4f0546f4b1ee2ceba82350de662d95d4
ibrahim <ibrahimsaad6677@gmail.com>
2024-12-24 12:56:22 +0200

    UPdate

--- src/Redux/OnlyBrandSlice.js
+++ src/Redux/OnlyBrandSlice.js
@@ -27,11 +27,11 @@ const OnlyBrandSlice = createSlice({
     },
 
     // Remove a product from the cart
-    removeFromCart: (state, action) => {
-      state.products = state.products.filter(
-        (product) => product.id !== action.payload
-      );
-    },
+    // removeFromCart: (state, action) => {
+    //   state.products = state.products.filter(
+    //     (product) => product.id !== action.payload
+    //   );
+    // },
 
     // Add a product to the wishlist
     addToWishlist: (state, action) => {
@@ -44,11 +44,11 @@ const OnlyBrandSlice = createSlice({
     },
 
     // Remove a product from the wishlist
-    // removeFromWishlist: (state, action) => {
-    //   state.favoriteproducts = state.favoriteproducts.filter(
-    //     (product) => product.id !== action.payload
-    //   );
-    // },
+    removeFromWishlist: (state, action) => {
+      state.favoriteproducts = state.favoriteproducts.filter(
+        (product) => product.id !== action.payload
+      );
+    },
 
     // Update the wishlist with a new list of products
     updateWithWishlist: (state, action) => {
